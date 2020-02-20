361b0b5066b466da7a47d1547bf3efc75c655083
nloyfer@gmail.com <nloyfer@gmail.com>
2020-02-20 13:23:43 +0200

    min_len

--- vis.py
+++ vis.py
@@ -21,6 +21,8 @@ def parse_args():   # todo: seperate args parsing for beta and pat
                                                               'Only relevant for pat files.')
     parser.add_argument('--max_reps', '-m', type=int, default=10, help='Pat vis: Display a read at most "max_reps" times, '
                                                                  'if it is repeating itself. [10]')
+    parser.add_argument('--min_len', type=int, default=1,
+                        help='Pat vis: Display only reads covering at least MIN_LEN CpG sites [1]')
     parser.add_argument('--color_scheme', '-cs', type=int, default=256,
                         help='beta vis: Color scheme. Possible values: 16 or 256 [256]')
     parser.add_argument('--plot', action='store_true', help='beta vis: plot results in a heatmap.')
@@ -46,7 +48,7 @@ def main():
     if args.title:
         print('{}'.format(args.title))
 
-    if file_type == '.beta':
+    if file_type in ('.beta', '.bin'):
         beta_vis_main(args)
     elif file_type == '.pat.gz':
         pat_vis_main(args)
