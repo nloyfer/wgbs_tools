6a69bba7fb1d6cb40215f6b2f6d3ebd6e2183ace
nloyfer@gmail.com <nloyfer@gmail.com>
2020-03-01 17:59:48 +0200

    fixed bugs

--- merge.py
+++ merge.py
@@ -9,7 +9,6 @@ import os.path as op
 import sys
 import os
 from index_wgbs import Indxer
-import pandas as pd
 
 
 class MergePats:
@@ -18,115 +17,34 @@ class MergePats:
         self.out_nogzip = out_nogzip
         self.labels = labels
 
-    def merge_pats(self, fast=False):
-        self.fast_merge_pats(None) if fast else self.slow_merge_pats()
+    def merge_pats(self):
+        self.fast_merge_pats()
 
-    def slow_merge_pats(self):
-        """
-        Merge all pats as follows:
-            - decompress all pats to temporary files.
-            - Merge-sort them to a united uncompressed pat file with "almost duplicated" lines
-            - collapse "almost duplicated" lines
-            - bgzip and index result
-        :return: 0 iff success
-        """
-
-        # gunzip all parts:
-        tmps = []
-        for pat in self.pats:
-            pat_tmp = op.splitext(pat)[0] + '.tmp'  # i.e "part.pat.tmp"
-            tmps.append(pat_tmp)
-            subprocess.call('gunzip -cd {} > {}'.format(pat, pat_tmp), shell=True)
-
-        # merge-sort all pats:
-        merge_sort_open_pats(tmps, self.out_nogzip)
-
-    def compose_view_cmd(self, i, view_flags, labels):
+    def compose_view_cmd(self, i, view_flags):
         if view_flags is None:
             view_cmd = ' <(gunzip -c'
         else:
             view_cmd = ' <({wt} view {flags}'.format(wt=main_script, flags=view_flags[i])
         view_cmd += ' {}'.format(self.pats[i])
         tagcmd = ''
-        if labels is not None:
-            tagcmd = ' | sed s/\$/\'\t\'{}/'.format(labels[i])
+        if self.labels is not None:
+            tagcmd = ' | sed s/\$/\'\t\'{}/'.format(self.labels[i])
         view_cmd += '{})'.format(tagcmd)
         return view_cmd
 
     def fast_merge_pats(self, view_flags=None):
-        """
-        Use bash script, not intermediate files
-        """
+        """ Use piping and sort -m to merge pat files w/o intermediate files """
         cmd = 'sort -m -k2,2n -k3,3'
         for i in range(len(self.pats)):
-            cmd += self.compose_view_cmd(i, view_flags, self.labels)
+            cmd += self.compose_view_cmd(i, view_flags)
         cmd += ' | {} - '.format(collapse_pat_script)
         cmd += ' | bgzip > {}.gz'.format(self.out_nogzip)
         cmd = '/bin/bash -c "{}"'.format(cmd)
         subprocess.check_call(cmd, shell=True)
+
         if not op.isfile(self.out_nogzip + '.gz'):
             raise IllegalArgumentError('Error: failed to create file {}.gz'.format(self.out_nogzip))
         Indxer(self.out_nogzip + '.gz', force=True).run()
-
-
-def merge_sort_open_pats(open_pats, merged_path, remove_open_pats=True):
-    """
-    Merge-sort a list of pat files into one pat. Then bgzip and index it.
-    Remove the open_pats
-    :param open_pats: list of sorted pat files, not compressed.
-    :param merged_path: final output pat path, not compressed (ends with 'pat')
-    :return: path of compressed merged pat
-    """
-    # merge-sort all pats:
-    pat_with_dups = merged_path + '.with_dups'  # i.e "prefix.pat.with_dups"
-    cmd = 'sort -m -k2,2n -k3,3 ' + ' '.join(open_pats)
-    cmd += ' -o {} '.format(pat_with_dups)
-    subprocess.check_call(cmd, shell=True)
-
-    # remove the open_pats files
-    if remove_open_pats:
-        for tmp in open_pats:
-            if op.isfile(tmp):
-                os.remove(tmp)
-
-    # collapse "almost duplicated" lines:   # todo: use python instead of perl. pandas duplicates, then loop.
-    subprocess.check_call('{} {} > {}'.format(collapse_pat_script, pat_with_dups, merged_path), shell=True)
-
-    # df = pd.read_csv(pat_with_dups, sep='\t', header=None)
-    # collapse_adup_df(df)
-    os.remove(pat_with_dups)
-
-    # bgzip and index the output
-    Indxer(merged_path, force=True).run()
-
-    return merged_path + '.gz'
-
-
-# def compose_cols_names(pat_path, names):
-#    peek = pd.read_csv(pat_path, sep='\t', header=None, nrows=1)
-#    i = 0
-#    while len(peek.columns) > len(names):
-#        names += ['tag{}'.format(i)]
-#        i += 1
-#    return names
-
-
-def collapse_adup_df(df):
-    """ given a dataframe, collapse almost duplicated lines and return it """  #
-    print(df)
-    names_to_test = list(df.columns).remove(2)
-    bs = df.duplicates(subset=names_to_test)
-    print(bs)
-    print(df)
-    return df
-
-
-# def collapse_adup(pat_in, pat_out):
-#    for cf in df_generator(pat_in, 10):
-#        collapse_adup_df(cf)
-#        cf.to_csv(pat_out, sep='\t', header=None, index=None, mode='a')
-#    # todo: bgzip and index
-#    return
 
 
 def merge_betas(betas, opath):
@@ -146,32 +64,6 @@ def merge_betas(betas, opath):
     return data
 
 
-# class MergeUnqs:
-#     def __init__(self, unqs, out_nogzip):
-#         self.unqs = unqs
-#         self.out_nogzip = out_nogzip
-#
-#     def compose_view_cmd(self, i, view_flags):
-#         if view_flags is None:
-#             return ' <(gunzip -cd {p})'.format(p=self.unqs[i])
-#         else:
-#             return ' <(wgbs_tools view {flags} {p})'.format(p=self.unqs[i], flags=view_flags[i])
-#
-#     def fast_merge_unqs(self, view_flags=None):
-#         """
-#         Use bash script, with no intermediate files
-#         """
-#         cmd = 'sort -m -k2,2n -k3,3'
-#         for i in range(len(self.unqs)):
-#             cmd += self.compose_view_cmd(i, view_flags)
-#         cmd +=  ' | {} - '.format(collapse_pat_script)
-#         # cmd += ' | bgzip > {o}.gz && tabix -C -b 2 -e 2 {o}.gz'.format(o=self.out_nogzip)
-#         cmd += ' | bgzip > {}.gz'.format(self.out_nogzip)
-#         cmd = '/bin/bash -c "{}"'.format(cmd)
-#         # print(cmd)
-#         subprocess.check_call(cmd, shell=True)
-#         Indxer(self.out_nogzip + '.gz', force=True).run()
-
 def merge_unqs():
     raise NotImplemented
 
@@ -181,7 +73,6 @@ def parse_args():
     parser.add_argument('input_files', nargs='+')
     parser.add_argument('-p', '--prefix', help='Prefix of output file', required=True)
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite existing file if existed')
-    parser.add_argument('--fast', action='store_true', help='pat: Use bash for better performance')
     # parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('--labels', nargs='+', help='labels for the mixed reads. '
                                                     'Default is None')
@@ -214,7 +105,7 @@ def main():
     if files_type in ('beta', 'bin'):
         merge_betas(input_files, out_path)
     elif files_type == 'pat.gz':
-        MergePats(input_files, args.prefix + '.pat', labels).merge_pats(args.fast)
+        MergePats(input_files, args.prefix + '.pat', labels).merge_pats()
     elif files_type == 'unq.gz':
         merge_unqs()
     else:
