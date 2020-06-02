b0ac02e9bea1620fff5315ed6996e7b5a174e477
nloyfer@gmail.com <nloyfer@gmail.com>
2020-06-02 18:17:45 +0300

    duplicated blocks removed

--- beta_to_blocks.py
+++ beta_to_blocks.py
@@ -9,14 +9,14 @@ from utils_wgbs import validate_files_list
 from multiprocessing import Pool
 from os.path import splitext, basename
 import sys
-from utils_wgbs import load_beta_data, trim_to_uint8, default_blocks_path, eprint, GenomeRefPaths
+from utils_wgbs import load_beta_data, trim_to_uint8, default_blocks_path, eprint, GenomeRefPaths, splitextgz
 
 
 def get_bins(df, genome):
     end = GenomeRefPaths(genome).count_nr_sites() + 1   # 28217449 for hg19
-    arr = np.unique(np.concatenate([[1], df['ssite'], df['esite'], [end]]))
+    arr = np.unique(np.concatenate([[1], df['startCpG'], df['endCpG'], [end]]))
     arr.sort()
-    isin = np.isin(arr, np.concatenate([df['ssite'], [df['esite'][df.shape[0] - 1]]]))
+    isin = np.isin(arr, np.concatenate([df['startCpG'], [df['endCpG'][df.shape[0] - 1]]]))
     return arr - 1, isin
 
 
@@ -31,8 +31,8 @@ def apply_filter_wrapper(args, blocks_bins, finds, beta_path, df):
 
         # dump to file
         out_name = splitext(splitext(basename(args.blocks_file))[0])[0]
-        out_name = splitext(basename(beta_path))[0] + '_' + out_name + '.bin'
-        out_name = out_name.replace('_genome', '')
+        # out_name = splitext(basename(beta_path))[0] + '_' + out_name + '.bin'
+        out_name = splitext(basename(beta_path))[0] + '.bin'
         out_name = op.join(args.out_dir, out_name)
 
         trim_to_uint8(reduced_data).tofile(out_name)
@@ -53,6 +53,31 @@ def apply_filter_wrapper(args, blocks_bins, finds, beta_path, df):
         print('Exception:', e)
 
 
+def blocks_cleanup(df, args):
+    # remove duplicated blocks
+    nr_orig = df.shape[0]
+    df.drop_duplicates(inplace=True)
+    nr_non_dup = df.shape[0]
+    if nr_orig != nr_non_dup:
+        eprint('removed {:,} duplicated blocks'.format(nr_orig - nr_non_dup))
+    
+    # remove blocks with no CpGs
+    nr_removed = df[df.startCpG == df.endCpG].shape[0]
+    if nr_removed:
+        eprint('removed {:,} regions with no CpGs'.format(nr_removed))
+
+    if args.debug:
+        eprint(df[df.startCpG == df.endCpG])
+
+    df = df[df.startCpG < df.endCpG]
+
+    if df.shape[0] != nr_orig:
+        fixed_blocks_path = splitextgz(args.blocks_file)[0] + '.clean.bed.gz'
+        eprint('Dumping cleaned blocks file to', fixed_blocks_path)
+        df.to_csv(fixed_blocks_path, header=None, index=None, sep='\t', compression='gzip')
+    return df
+
+
 def main():
     """
     Collapse beta file to blocks binary file, of the same beta format
@@ -66,23 +91,16 @@ def main():
         eprint('Invalid blocks file:', args.blocks_file)
         return
 
-    names = ['chr', 'sloc', 'eloc', 'ssite', 'esite']
+    names = ['chr', 'start', 'end', 'startCpG', 'endCpG']
     df = pd.read_csv(args.blocks_file, sep='\t', usecols=[0, 1, 2, 3, 4], header=None, names=names)
+    df = blocks_cleanup(df, args)
 
-    nr_removed = df[df.ssite == df.esite].shape[0]
-    if nr_removed:
-        eprint('removed {} regions with no CpGs'.format(nr_removed))
-
-    if args.debug:
-        eprint(df[df.ssite == df.esite])
-
-    df = df[df.ssite < df.esite]
     blocks_bins, filtered_indices = get_bins(df, args.genome)
 
     with Pool() as p:
         for beta_path in files:
             params = (args, blocks_bins,
-                      filtered_indices, beta_path, df[['chr', 'sloc', 'eloc']])
+                      filtered_indices, beta_path, df[['chr', 'start', 'end']])
             p.apply_async(apply_filter_wrapper, params)
         p.close()
         p.join()
