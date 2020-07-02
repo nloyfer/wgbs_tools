2ff333fbf439c5d37b7b59885970a63cf5ea943b
nloyfer@gmail.com <nloyfer@gmail.com>
2020-07-02 18:34:25 +0300

    deal with messy blocks files

--- beta_to_blocks.py
+++ beta_to_blocks.py
@@ -9,7 +9,8 @@ from utils_wgbs import validate_files_list
 from multiprocessing import Pool
 from os.path import splitext, basename
 import sys
-from utils_wgbs import load_beta_data, trim_to_uint8, default_blocks_path, eprint, GenomeRefPaths, splitextgz
+from utils_wgbs import load_beta_data, trim_to_uint8, default_blocks_path, eprint, GenomeRefPaths, splitextgz, \
+                        IllegalArgumentError
 
 
 def get_bins(df, genome):
@@ -30,13 +31,13 @@ def apply_filter_wrapper(args, blocks_bins, finds, beta_path, df):
         reduced_data = np.add.reduceat(data, blocks_bins)[finds][:-1]
 
         # dump to file
+        suff = '.lbeta' if args.lbeta else '.bin'
         out_name = splitext(splitext(basename(args.blocks_file))[0])[0]
-        # out_name = splitext(basename(beta_path))[0] + '_' + out_name + '.bin'
-        out_name = splitext(basename(beta_path))[0] + '.bin'
+        out_name = splitext(basename(beta_path))[0] + suff
         out_name = op.join(args.out_dir, out_name)
 
-        trim_to_uint8(reduced_data).tofile(out_name)
-        print(out_name)
+        trim_to_uint8(reduced_data, args.lbeta).tofile(out_name)
+        eprint(out_name)
 
         if args.bedGraph:
             with np.errstate(divide='ignore', invalid='ignore'):
@@ -44,7 +45,7 @@ def apply_filter_wrapper(args, blocks_bins, finds, beta_path, df):
                 eprint(beta_vals.shape, df.shape)
             # beta_vals[reduced_data[:, 1] == 0] = np.nan
             df['beta'] = beta_vals
-            df.to_csv(out_name.replace('.bin', '.bedGraph'), sep='\t',
+            df.to_csv(out_name.replace(suff, '.bedGraph'), sep='\t',
                       index=None, header=None, na_rep=-1,
                       float_format='%.2f')
 
@@ -75,8 +76,92 @@ def blocks_cleanup(df, args):
         fixed_blocks_path = splitextgz(args.blocks_file)[0] + '.clean.bed.gz'
         eprint('Dumping cleaned blocks file to', fixed_blocks_path)
         df.to_csv(fixed_blocks_path, header=None, index=None, sep='\t', compression='gzip')
+
     return df
 
+def validate_block_file(df):
+    # file has >=5 columns
+    # no duplicated lines
+    # sorted by 4'th column (startCpG)
+    # no overlap between regions
+    return
+
+# def is_blocks_file_nice(df):
+    # nice = True
+    # # see if the blocks file is "nice"
+
+    # # startCpG and endCpG is monotonically increasing
+    # if !pd.Index(df['startCpG']).is_monotonic_increasing():
+        # eprint('startCpG is not monotonically increasing')
+        # return False
+    # if !pd.Index(df['endCpG']).is_monotonic_increasing():
+        # eprint('endCpG is not monotonically increasing')
+        # return False
+
+    # if not (np.diff(df['endCpG'] - df['startCpG']) > 0).all():
+        # eprint('Some blocks don\'t cover any CpGs')
+
+def load_blocks_file(blocks_path):
+    if not op.isfile(blocks_path):
+        eprint('Invalid blocks file:', blocks_path)
+        raise IllegalArgumentError('No blocks file')
+
+    names = ['chr', 'start', 'end', 'startCpG', 'endCpG']
+    df = pd.read_csv(blocks_path, sep='\t', usecols=[0, 1, 2, 3, 4], header=None, names=names)
+    return df
+    # starts = df['startCpG']
+    # ends = df['endCpG']
+
+    # # blocks file is valid
+    # if not (np.diff(df['endCpG'] - df['startCpG']) >= 0).all():
+        # eprint('Invalid blocks file: endCpG>=startCpG is required')
+        # raise IllegalArgumentError('Invalid blocks file')
+
+
+
+def slow_but_robust(df, args):
+    for beta_path in args.input_files:
+
+        try:
+            res = np.zeros((df.shape[0], 2), dtype=int)
+
+            # load beta file:
+            data = load_beta_data(beta_path)
+
+            # reduce to blocks:
+            i = -1
+            for _, row in df.iterrows():
+                i += 1
+                startCpG = row[3]
+                endCpG = row[4]
+                line = np.sum(data[startCpG - 1:endCpG - 1, :], axis=0)
+                res[i, :] = line
+
+            # dump to file
+            suff = '.lbeta' if args.lbeta else '.bin'
+            out_name = splitext(splitext(basename(args.blocks_file))[0])[0]
+            out_name = splitext(basename(beta_path))[0] + suff
+            out_name = op.join(args.out_dir, out_name)
+
+            trim_to_uint8(res, args.lbeta).tofile(out_name)
+            eprint(out_name)
+
+            if args.bedGraph:
+                with np.errstate(divide='ignore', invalid='ignore'):
+                    beta_vals = res[:, 0] / res[:, 1]
+                    eprint(beta_vals.shape, df.shape)
+                # beta_vals[res[:, 1] == 0] = np.nan
+                df['beta'] = beta_vals
+                df.to_csv(out_name.replace(suff, '.bedGraph'), sep='\t',
+                          index=None, header=None, na_rep=-1,
+                          float_format='%.2f')
+
+        except Exception as e:
+            print('Failed with beta', beta_path)
+            print('Exception:', e)
+
+
+    return
 
 def main():
     """
@@ -87,12 +172,13 @@ def main():
     files = args.input_files
     validate_files_list(files, '.beta')
 
-    if not op.isfile(args.blocks_file):
-        eprint('Invalid blocks file:', args.blocks_file)
+    # load blocks:
+    df = load_blocks_file(args.blocks_file)
+
+    if args.slow:
+        slow_but_robust(df, args)
         return
 
-    names = ['chr', 'start', 'end', 'startCpG', 'endCpG']
-    df = pd.read_csv(args.blocks_file, sep='\t', usecols=[0, 1, 2, 3, 4], header=None, names=names)
     df = blocks_cleanup(df, args)
 
     blocks_bins, filtered_indices = get_bins(df, args.genome)
@@ -114,8 +200,10 @@ def parse_args():
     parser.add_argument('input_files', nargs='+', help='one or more beta files')
     parser.add_argument('-b', '--blocks_file', help='blocks path', default=default_blocks_path)
     parser.add_argument('-o', '--out_dir', help='output directory. Default is "."', default='.')
+    parser.add_argument('-l', '--lbeta', action='store_true', help='Use lbeta file (uint16) instead of bin (uint8)')
     parser.add_argument('--bedGraph', action='store_true', help='output a text file in addition to binary file')
     parser.add_argument('--debug', '-d', action='store_true')
+    parser.add_argument('--slow', action='store_true', help='slower but more robust implementation. Use this flag when input blocks file contains overlapping blocks')
     parser.add_argument('--genome', help='Genome reference name. Default is hg19.', default='hg19')
 
     return parser.parse_args()
