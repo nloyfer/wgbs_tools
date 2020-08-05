20186d4782f6872425c1524d76a83745c29b88f3
yonniejon <jonathanrosenski@gmail.com>
2020-08-05 15:12:46 +0300

    updates to add md

--- bamAddMethylData.py
+++ bamAddMethylData.py
@@ -6,9 +6,10 @@ import argparse
 import subprocess
 import shlex
 import re
+import datetime
 import multiprocessing
 from multiprocessing import Pool
-from utils_wgbs import IllegalArgumentError, patter_tool, add_GR_args, eprint
+from utils_wgbs import IllegalArgumentError, patter_tool, match_maker_tool, add_GR_args, eprint
 from init_genome_ref_wgbs import chromosome_order
 from genomic_region import GenomicRegion
 
@@ -18,7 +19,7 @@ BAM_SUFF = '_md.bam'
 # Minimal Mapping Quality to consider.
 # 10 means include only reads w.p. >= 0.9 to be mapped correctly.
 # And missing values (255)
-MAPQ = 0
+MAPQ = 10
 
 FLAGS_FILTER = 1796  # filter flags with these bits
 # todo: unsorted / sorted by name
@@ -39,35 +40,35 @@ def subprocess_wrap(cmd, debug):
             raise IllegalArgumentError('Failed')
 
 
-def proc_chr(input_path, out_path, region, genome, header_path, debug):
+def proc_chr(input_path, out_path_name, region, genome, header_path, paired_end, debug):
     """ Convert a temp single chromosome file, extracted from a bam file,
         into a sam formatted (no header) output file."""
 
     # Run patter tool 'bam' mode on a single chromosome
 
+    unsorted_bam = out_path_name + '_unsorted.output.bam'
+    out_path = out_path_name + '.output.bam'
+    out_directory = os.path.dirname(out_path)
+
     # use samtools to extract only the reads from 'chrom'
     flag = '-f 3'
     cmd = "samtools view {} {} -q {} -F 1796 {} | ".format(input_path, region, MAPQ, flag)
     if debug:
         cmd += ' head -200 | '
-    cmd += "{} {} {} bam | cat {} - | samtools view -b -h - > {}".format(patter_tool, genome.genome_path,
+    if paired_end:
+        # change reads order, s.t paired reads will appear in adjacent lines
+        cmd += "{} | ".format(match_maker_tool)
+    cmd += "{} {} {} bam | cat {} - > {}".format(patter_tool, genome.genome_path,
                                                                             genome.chrom_cpg_sizes,
-                                                                            header_path, out_path)
+                                                                            header_path, unsorted_bam)
+
+    sort_cmd = 'samtools sort -o {} -T {} {}'.format(out_path, out_directory, unsorted_bam)
+
     #print(cmd)
     subprocess_wrap(cmd, debug)
-
+    subprocess_wrap(sort_cmd, debug)
+    os.remove(unsorted_bam)
     return out_path
-
-def proc_chr_cmd(input_path, region, genome, debug):
-
-    # use samtools to extract only the reads from 'chrom'
-    flag = '-f 3'
-    cmd = "samtools view {} {} -q {} -F 1796 {} | ".format(input_path, region, MAPQ, flag)
-    if debug:
-        cmd += ' head -200 | '
-    cmd += "{} {} {} bam".format(patter_tool, genome.genome_path, genome.chrom_cpg_sizes)
-
-    return cmd
 
 def get_header_command(input_path):
     return "samtools view -H {}".format(input_path)
@@ -139,6 +140,10 @@ class BamMethylData:
 
             return chroms
 
+    def is_pair_end(self):
+        first_line = subprocess.check_output('samtools view {} | head -1'.format(self.bam_path), shell=True)
+        return int(first_line.decode().split('\t')[1]) & 1
+
     def intermediate_bam_file_view(self, name):
         return '<(samtools view {})'.format(name)
 
@@ -148,15 +153,15 @@ class BamMethylData:
     def start_threads(self):
         """ Parse each chromosome file in a different process,
             and concatenate outputs to pat and unq files """
-
+        print(datetime.datetime.now().isoformat() + ": *** starting processing of each chromosome")
         name = op.join(self.out_dir, op.basename(self.bam_path)[:-4])
         header_path = name + '.header'
         proc_header(self.bam_path, header_path, self.debug)
         processes = []
         with Pool(self.args.threads) as p:
             for c in self.set_regions():
-                out_path = name + '_' + c + '.output.bam'
-                params = (self.bam_path, out_path, c, self.gr.genome, header_path, self.debug)
+                out_path_name = name + '_' + c
+                params = (self.bam_path, out_path_name, c, self.gr.genome, header_path, self.is_pair_end(), self.debug)
                 processes.append(p.apply_async(proc_chr, params))
             if not processes:
                 raise IllegalArgumentError('Empty bam file')
@@ -168,36 +173,35 @@ class BamMethylData:
             print('threads failed')
             return
 
+        print(datetime.datetime.now().isoformat() + ": finished processing each chromosome")
         # Concatenate chromosome files
         final_path = name + BAM_SUFF
+        out_directory = os.path.dirname(final_path)
         # cmd = '/bin/bash -c "cat <({})'.format(get_header_command(self.bam_path)) + ' ' +\
-        #       ' '.join([self.intermediate_bam_file_view(p) for p in res]) + ' | samtools view -b - > ' + final_path + '"'
-        cmd = '/bin/bash -c "samtools cat -h <({})'.format(get_header_command(self.bam_path)) + ' ' + \
-              ' '.join(
-                  [p for p in res]) + ' > ' + final_path + '"'
-        print('starting cat of files')
+        #       ' '.join([self.intermediate_bam_file_view(p) for p in res]) + ' | samtools view -b - > ' + final_path_unsorted + '"'
+        cmd ="samtools merge -h {} {} ".format(header_path, final_path) + ' '.join([p for p in res])
+        # cmd = '/bin/bash -c "samtools cat -h <({})'.format(get_header_command(self.bam_path)) + ' ' + \
+        #       ' '.join(
+        #           [p for p in res]) + ' > ' + final_path + '"'
+        print(datetime.datetime.now().isoformat() + ': starting cat of files')
         process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        print("finished cat of files")
+        print(datetime.datetime.now().isoformat() + ": finished cat of files")
+
+        # sort_cmd = 'samtools sort -o {} -T {} {}'.format(final_path, out_directory, final_path_unsorted)
+        # print(datetime.datetime.now().isoformat() + ': starting sort of file')
+        # sort_process = subprocess.Popen(shlex.split(sort_cmd), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        # stdout, stderr = sort_process.communicate()
+        # print(datetime.datetime.now().isoformat() + ": finished sort of file")
+
+        idx_command = "samtools index {}".format(final_path)
+        print('starting index of output bam ' + datetime.datetime.now().isoformat())
+        idx_process = subprocess.Popen(shlex.split(idx_command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        stdout, stderr = idx_process.communicate()
+        print(datetime.datetime.now().isoformat() + ": finished index of output bam")
         res.append(header_path)
         # remove all small files
         list(map(os.remove, [l for l in res]))
-
-    def run_cmd(self):
-        """ Parse each chromosome file in a different process,
-            and concatenate outputs to pat and unq files """
-
-        name = op.join(self.out_dir, op.basename(self.bam_path)[:-4])
-        chr_cmds = []
-        for c in self.set_regions():
-            chr_cmds.append(proc_chr_cmd(self.bam_path, c, self.gr.genome, False))
-
-        # Concatenate the outputs of the cmds files
-        final_path = name + BAM_SUFF
-        res_cmds = [get_header_command(self.bam_path)] + chr_cmds
-        cmd = '/bin/bash -c "cat ' + ' '.join([self.process_substitute(p) for p in res_cmds]) + ' | samtools view -b - > ' + final_path + '"'
-        process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        _, _ = process.communicate()
 
 
 def parse_args():
