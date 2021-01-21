d3fd790edcbefd865aed29b730fe4de8693e368f
nloyfer@gmail.com <nloyfer@gmail.com>
2021-01-21 12:17:24 +0200

    BrokenPipeError

--- utils_wgbs.py
+++ utils_wgbs.py
@@ -375,8 +375,5 @@ def read_shell(command, **kwargs):
 
 
 def catch_BrokenPipeError():
-    # Python flushes standard streams on exit; redirect remaining output
-    # to devnull to avoid another BrokenPipeError at shutdown
-    devnull = os.open(os.devnull, os.O_WRONLY)
-    os.dup2(devnull, sys.stdout.fileno())
-    sys.exit(1)  # Python exits with error code 1 on EPIPE
+    os.dup2(os.open(os.devnull, os.O_WRONLY), sys.stdout.fileno())
+    sys.exit(1)
