f11e6c3d7f98e15d38e31eff877bfe872d079b54
nloyfer@gmail.com <nloyfer@gmail.com>
2021-03-08 10:37:28 +0200

    change from unq to pat

--- frag_len.py
+++ frag_len.py
@@ -13,31 +13,29 @@ import os.path as op
 
 
 def awk_wrap(awk_cmd):
+    # print(awk_cmd)
     x = subprocess.check_output(awk_cmd, shell=True).decode()
     return np.array([int(r) if r else 0 for r in x.split('\n')[:-1]], dtype=int, ndmin=2)
 
 
 class FragLen:
-    def __init__(self, unq, args, gr=None):
+    def __init__(self, pat, args, gr=None):
         self.args = args
-        self.unq = unq
+        self.pat = pat
         self.gr = gr if gr else GenomicRegion(args)
         m = args.max_frag_size
-        self.fill_arr_cmd = ' {if ($3 > %s) {$3 = %s}; arr[$3] += $5}' % (m, m)
-        self.print_arr_cmd = ' END {for (x=1; x <= %s; x++) print arr[x]}\'' % m
+        self.awk_hist_cmd = ' | awk \'{l=length($3) ; if (l > %s) {l = %s}; arr[l] += $4} END {for (x=1; x <= %s; x++) print arr[x]}\'' % (m, m, m)
 
     def run_small_region(self):
-        start, end = self.gr.bp_tuple
-        cmd = 'tabix {} '.format(self.unq)
-        cmd += '{}:{}-{} '.format(self.gr.chrom, max(1, int(start) - MAX_READ_LEN), end)
-        cmd += ' | awk \'{if (($2 + $3) > %s) ' % start
-        cmd += self.fill_arr_cmd + '}' + self.print_arr_cmd
+        cmd = f'wgbs_tools cview {self.pat} -r {self.gr.region_str} {self.awk_hist_cmd}'
+        return awk_wrap(cmd)
+
+    def run_bed(self):
+        cmd = f'wgbs_tools cview {self.pat} -L {self.args.bed_file} {self.awk_hist_cmd}'
         return awk_wrap(cmd)
 
     def chrom_cmd(self, chrom):
-        cmd = 'tabix {} {} | awk \''.format(self.unq, chrom)
-        cmd = cmd + self.fill_arr_cmd + self.print_arr_cmd
-        return cmd
+        return f'tabix {self.pat} {chrom} {self.awk_hist_cmd}'
 
     def run_whole_genome(self):
         chroms = list(pd.read_csv(self.gr.genome.chrom_sizes, sep='\t', header=None, usecols=[0])[0])
@@ -48,47 +46,53 @@ class FragLen:
         return np.sum(np.concatenate([r.get() for r in pr]), axis=0)
 
 
-def compose_fig_path(unq, outdir, grs):
-    if not outdir:
-        return
-    res = op.join(outdir, op.basename(splitextgz(unq)[0]))
-    if grs and len(grs) == 1:
-        res += '.{}'.format(grs[0].region_str)
-    res += '.png'
-    return res
+def compose_fig_path(pat, outdir):
+    if outdir:
+        return op.join(outdir, op.basename(splitextgz(pat)[0])) + '.png'
 
 
-def run_single_unq(unq, grs, args):
-    eprint(unq)
-    if not grs:  # process the whole unq file (no -L,-s,-r was specified)
-        x = FragLen(unq, args).run_whole_genome()
+def plot_hist(data, max_frag_size, pat):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(1, data.size + 1), data)
+    major_ticks = np.arange(1, max_frag_size, 5)
+    minor_ticks = np.arange(1, max_frag_size, 1)
+
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+
+    # Or if you want different settings for the grids:
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+    plt.ylim(bottom=0)
+    plt.xlim(left=1)
+    plt.title('Fragment lengths (CpGs)\n' + op.basename(splitextgz(pat)[0]))
+
+
+def run_single_pat(pat, args):
+    eprint(pat)
+    if args.region or args.sites:
+        x = FragLen(pat, args, GenomicRegion(args)).run_small_region()
+    elif args.bed_file:
+        x = FragLen(pat, args).run_bed()
     else:
-        x = np.sum([FragLen(unq, args, gr).run_small_region() for gr in grs], axis=0)
+        x = FragLen(pat, args).run_whole_genome()
 
     # print values to stdout:
     if args.verbose:
         np.savetxt(sys.stdout, x.reshape((1, -1)), fmt='%s', delimiter=' ')
 
     # plot:
-    plt.figure()
-    plt.plot(np.arange(x.size), x.flatten())
-    plt.title('Fragment lengths\n' + op.basename(splitextgz(unq)[0]))
+    plot_hist(x.flatten(), args.max_frag_size, pat)
 
     # dump figure:
     if args.outdir:
-        plt.savefig(compose_fig_path(unq, args.outdir, grs))
+        plt.savefig(compose_fig_path(pat, args.outdir))
 
 
 def multi_FragLen(args):
-    if args.region or args.sites:
-        grs = [GenomicRegion(args)]
-    elif args.bed_file:
-        grs = BedFileWrap(args.bed_file).iter_grs()
-    else:
-        grs = []
-
-    for unq in args.unq_paths:
-        run_single_unq(unq, grs, args)
+    for pat in args.pat_paths:
+        run_single_pat(pat, args)
 
     if args.display:
         plt.show()
@@ -103,10 +107,10 @@ def multi_FragLen(args):
 
 def parse_args():
     parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument('unq_paths', nargs='+')
+    parser.add_argument('pat_paths', nargs='+')
     parser.add_argument('-v', '--verbose', action='store_true', help='print the histogram values to stdout')
-    parser.add_argument('-m', '--max_frag_size', type=int, default=500,
-                        help='Maximum fragment size. Longer fragments will be trimmed. [500]')
+    parser.add_argument('-m', '--max_frag_size', type=int, default=30,
+                        help='Maximum fragment size. Longer fragments will be trimmed. [30]')
     parser.add_argument('--outdir', '-o', help='output directory for the histogram figure(s) [None]')
     parser.add_argument('--display', action='store_true', help='Display histogram plot(s) (plt.show)')
     add_GR_args(parser, bed_file=True)
@@ -115,11 +119,12 @@ def parse_args():
 
 def main():
     """
-    Plot histogram of reads lengths of unq file
+    Plot histogram of reads lengths (in sites) of pat file
     Output to stdout the histogram values if requested
     """
     args = parse_args()
-    validate_file_list(args.unq_paths, 'unq.gz')
+    # validate_file_list(args.pat_paths, '.pat.gz')
+    validate_file_list(args.pat_paths, '.pat.gz')
     multi_FragLen(args)
 
 
