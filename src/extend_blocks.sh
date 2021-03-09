95c751e2c0092f07983e36f9ed589bc306802578
nloyfer@gmail.com <nloyfer@gmail.com>
2021-03-09 14:40:32 +0200

    cview remove temp file

---
+++ src/extend_blocks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cut -f1,4,5 | sort -k2,2n | \
+awk -v OFS="\t" '
+{
+    start = $2 - 100;
+    if (start < 1) {
+        start = 1;
+    }
+    print $1,start,$3
+}
+' | bedtools merge -i - -sorted
