7772eecb4559ce1358a2119d31642c95814b153f
nloyfer@gmail.com <nloyfer@gmail.com>
2021-07-11 11:48:34 +0300

    eprint, validation, todos

--- beta_to_450k.py
+++ beta_to_450k.py
@@ -5,10 +5,13 @@ import sys
 import numpy as np
 import os.path as op
 import pandas as pd
-from utils_wgbs import validate_file_list, load_beta_data, beta2vec, IllegalArgumentError, eprint
+from utils_wgbs import validate_single_file, validate_file_list, load_beta_data,
+                       beta2vec, IllegalArgumentError, eprint
 from multiprocessing import Pool, cpu_count
 
-ilmn2cpg_dict = '/cs/cbio/netanel/indexes/ilmn2CpG.tsv.gz'  # todo: generate this and put in reference directory?
+ilmn2cpg_dict = '/cs/cbio/netanel/indexes/ilmn2CpG.tsv.gz'
+# todo: generate this and put in reference directory. download from:
+# https://support.illumina.com/array/array_kits/infinium-methylationepic-beadchip-kit/downloads.html
 
 
 def single_beta(beta_path, indices, cov_thresh):
@@ -18,13 +21,13 @@ def single_beta(beta_path, indices, cov_thresh):
 
 def read_reference(ref):
     # read Illumina-to-CpG_Index table:
+    validate_single_file(ilmn2cpg_dict)
     df = pd.read_csv(ilmn2cpg_dict, sep='\t', header=None, names=['ilmn', 'cpg'])
     if ref is None:
         return df
 
     # validate and read reference file
-    if not op.isfile(ref):
-        raise IllegalArgumentError('No such file: {}'.format(ref))
+    validate_single_file(ref)
     rf = pd.read_csv(ref, header=None, usecols=[0], names=['ilmn'])
     # remove first row if it's not a cg entry:
     if pd.isna(rf['ilmn'][0]) or not rf['ilmn'][0].startswith('cg'):
@@ -40,8 +43,8 @@ def read_reference(ref):
         msg = 'WARNING: Skipping some unrecognized Illumina IDs \n'
         msg += '(not found in the map table {})\n'.format(ilmn2cpg_dict)
         if not missing_sites['ilmn'].empty:
-            print(missing_sites['ilmn'])
-            print(list(missing_sites['ilmn']))
+            eprint(missing_sites['ilmn'])
+            eprint(list(missing_sites['ilmn']))
             msg += 'The missing sites: {}'.format(','.join(map(str, missing_sites['ilmn'])))
         eprint(msg)
     mf = mf[~mf['cpg'].isna()]
@@ -53,6 +56,7 @@ def read_reference(ref):
 def betas2csv(args):
     df = read_reference(args.ref)
     indices = np.array(df['cpg'])
+    # indices = df['cpg'].values
 
     with Pool(cpu_count()) as p:
         resarr = [p.apply_async(single_beta, (b, indices, args.cov_thresh)) for b in args.input_files]
@@ -63,7 +67,7 @@ def betas2csv(args):
         df[name] = beta
     df = df.sort_values(by='ilmn')
     del df['cpg']
-    df.to_csv(args.out_path, index=None, float_format='%.3f', na_rep='NaN')
+    df.to_csv(args.out_path, index=None, float_format='%.3f', na_rep='NA')
 
 
 def parse_args():
@@ -72,8 +76,10 @@ def parse_args():
     parser.add_argument('-o', '--out_path', type=argparse.FileType('w'), default=sys.stdout,
                         help='Output path. [stdout]')
     parser.add_argument('-c', '--cov_thresh', type=int, default=1,
-                        help='minimal coverage to include. sites with coverage < cov_thresh are ignored [1]')
-    parser.add_argument('--ref', help='a reference file with one column, of Illumina IDs, and no header.')
+                        help='minimal coverage to include. sites with coverage < ' \
+                        'cov_thresh are ignored [1]')
+    parser.add_argument('--ref', help='a reference file with one column, ' \
+                        'of Illumina IDs, and no header.')
     args = parser.parse_args()
     return args
 
@@ -83,7 +89,7 @@ def main():
     Convert beta file[s] to Illumina-450K format.
     Output: a csv file with ~480K rows, for the ~480K Illumina sites,
             and with columns corresponding to the beta files.
-            all values are in range [0, 1], or NaN.
+            all values are in range [0, 1], or NA.
     """
     args = parse_args()
     validate_file_list(args.input_files, '.beta')
