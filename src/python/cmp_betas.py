ad5f290a4d030100ea746c3d3a105ec708a80795
nloyfer@gmail.com <nloyfer@gmail.com>
2021-09-27 12:46:21 +0300

    dump option

--- src/python/cmp_betas.py
+++ src/python/cmp_betas.py
@@ -3,38 +3,48 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from utils_wgbs import load_beta_data, validate_file_list, add_GR_args
+from utils_wgbs import load_beta_data, validate_file_list, add_GR_args, eprint
 from genomic_region import GenomicRegion
 import os.path as op
 from itertools import combinations
 
 
-def comp2(a, b, ids, cov_thresh):
-
+def comp2(a, b, ids, cov_thresh, out_dir):
     # Ignore sites with coverage lower than cov_thresh in at
     # least one of the two compared files
     high_cov = np.min(np.c_[a[:, 1], b[:, 1]], axis=1) >= cov_thresh
-    x = np.copy(a[high_cov].astype(np.float16))
-    y = np.copy(b[high_cov].astype(np.float16))
-    x = x[:, 0] / x[:, 1]
-    y = y[:, 0] / y[:, 1]
+    def table2vec(table):
+        table = table[high_cov]
+        return table[:, 0] / table[:, 1]
+    plt.hist2d(table2vec(a), table2vec(b), bins=20, cmap=plt.cm.jet, norm=LogNorm())
     plt.title('{} : {}'.format(*ids), fontsize=10)
-    plt.hist2d(x, y, bins=20, cmap=plt.cm.jet, norm=LogNorm())
+    if out_dir is not None:
+        outpath = op.join(out_dir, '{}:{}.png'.format(*ids))
+        plt.savefig(outpath)
+        eprint(f'[wt cmp] dumped figure to {outpath}')
 
 
-def compare_all_paires(betas, min_cov, sites):
+# def compare_all_paires(betas, min_cov, sites):
+def compare_all_paires(args):
+    betas = args.betas
+    sites = GenomicRegion(args).sites
     tables = [load_beta_data(b, sites) for b in betas]
     names = [op.splitext(op.basename(b))[0] for b in betas]
 
     for x, y in combinations(range(len(tables)), r=2):
         plt.figure()
-        comp2(tables[x], tables[y], (names[x], names[y]), min_cov)
-    plt.show()
+        comp2(tables[x], tables[y], (names[x], names[y]), args.min_cov, args.out_dir)
+    if args.show or args.out_dir is None:
+        plt.show()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('betas', nargs='+')
+    parser.add_argument('--out_dir', '-o',
+            help='Dump figures to this directory. If not specified, --show flag is set')
+    parser.add_argument('--show', action='store_true',
+            help='Display the figures using matplotlib.pyplot.show.')
     parser.add_argument('--min_cov', '-c', type=int, default=10,
                         help='Minimal coverage to consider. '
                              'Sites with coverage lower than this value are ignored')
@@ -52,7 +62,8 @@ def main():
     """
     args = parse_args()
     validate_file_list(args.betas, '.beta', min_len=2)
-    compare_all_paires(args.betas, args.min_cov, GenomicRegion(args).sites)
+    # compare_all_paires(args.betas, args.min_cov, GenomicRegion(args).sites)
+    compare_all_paires(args)
 
 
 if __name__ == '__main__':
