3e5b9bf02f18b229545338529fcc6389a611653b
nloyfer@gmail.com <nloyfer@gmail.com>
2021-10-05 17:40:54 +0300

    mbias plot; small fixes

--- src/python/bam2pat.py
+++ src/python/bam2pat.py
@@ -4,14 +4,15 @@ import os
 import os.path as op
 import argparse
 import numpy as np
+import pandas as pd
 import subprocess
 import shutil
 import uuid
 import re
 from multiprocessing import Pool
 from utils_wgbs import IllegalArgumentError, match_maker_tool, patter_tool, \
-    add_GR_args, eprint, add_multi_thread_args, mult_safe_remove, \
-    GenomeRefPaths, validate_single_file, delete_or_skip, check_executable, EmptyBamError
+    add_GR_args, eprint, add_multi_thread_args, EmptyBamError \
+    GenomeRefPaths, validate_single_file, delete_or_skip, check_executable
 from init_genome_ref_wgbs import chromosome_order
 from pat2beta import pat2beta
 from index_wgbs import Indxer
@@ -63,15 +64,13 @@ def gen_pat_part(out_path, debug, temp_dir):
         cmd += f' | bgzip -f > {pat_path}'
         subprocess_wrap(cmd, debug)
 
-        mult_safe_remove([out_path])
-
         return pat_path
     except IllegalArgumentError as e:
         return None
 
 
 def proc_chr(bam, out_path, region, genome, chr_offset, paired_end, ex_flags, mapq, debug,
-             blueprint, temp_dir, blacklist, whitelist, min_cpg, verbose):
+             blueprint, temp_dir, blacklist, whitelist, min_cpg, mbias, verbose):
     """ Convert a temp single chromosome file, extracted from a bam file, into pat """
 
     # Run patter tool on a single chromosome. out_path will have the following fields:
@@ -105,6 +104,8 @@ def proc_chr(bam, out_path, region, genome, chr_offset, paired_end, ex_flags, ma
     region = extend_region(region, MAX_READ_SIZE)
     cmd += f' | {patter_tool} {genome.dict_path} {region}'
     cmd += f' --min_cpg {min_cpg}'
+    if mbias:
+        cmd += f' --mbias {out_path}.mb'
     cmd += f' > {out_path}'
     if verbose:
         print(cmd)
@@ -227,7 +228,8 @@ class Bam2Pat:
                 par = (self.bam_path, out_path, c, self.gr.genome, chr_offset,
                        is_pair_end(self.bam_path), self.args.exclude_flags,
                        self.args.mapq, self.args.debug, self.args.blueprint,
-                       self.args.temp_dir, blist, wlist, self.args.min_cpg, self.verbose)
+                       self.args.temp_dir, blist, wlist, self.args.min_cpg,
+                       self.args.mbias, self.verbose)
                 params.append(par)
 
             if len(cur_regions) == 1 and self.args.threads == 1:
@@ -248,12 +250,13 @@ class Bam2Pat:
             else:
                 raise e
 
+        self.mbias_merge(name, pat_parts)
         self.concat_parts(name, pat_parts)
-        # remove all small files
-        mult_safe_remove(pat_parts)
 
     def validate_parts(self, pat_parts):
         # validate parts:
+        pat_parts = [p for p in pat_parts if p]  # in case only some of the parts are empty
+
         for part in pat_parts:
             if not part:            # subprocess threw exception or no reads found
                 eprint('[wt bam2pat] threads failed')
@@ -265,6 +268,30 @@ class Bam2Pat:
             eprint('[wt bam2pat] No reads found in bam file. No pat file is generated')
             return False
         return True
+
+    def mbias_merge(self, name, pat_parts):
+        if not self.args.mbias:
+            return
+        try:
+            mdir = op.join(self.out_dir, name) + '.mbias'
+            if not op.isdir(mdir):
+                os.mkdir(mdir)
+            tpaths = []
+            for x in ['OB', 'OT']:
+                mbias_parts = [p.replace('.pat.gz', f'.mb.{x}.txt') for p in pat_parts if p]
+                mbias_parts = [pd.read_csv(m, sep='\t') for m in mbias_parts]
+                df = mbias_parts[0]
+                for m in mbias_parts[1:]:
+                    df += m
+                cpath = op.join(mdir, name) + f'.mbias.{x}.txt'
+                df.to_csv(cpath, sep='\t', index=None)
+                tpaths.append(cpath)
+            from mbias_plot import plot_mbias
+            plot_mbias(tpaths, mdir)
+        except Exception as e:
+            eprint('[wt bam2pat] failed in mbias')
+            eprint(e)
+
 
     def concat_parts(self, name, pat_parts):
 
@@ -279,11 +306,12 @@ class Bam2Pat:
             eprint(f'[wt bam2pat] failed to generate {pat_path}')
             return
 
-        # generate beta file and index the pat file
+        # index the pat file (.csi)
         eprint('[wt bam2pat] indexing and generating beta file...')
         Indxer(pat_path, threads=self.args.threads).run()
         eprint(f'[wt bam2pat] generated {pat_path}')
 
+        # generate beta file
         if not self.args.no_beta:
             beta_path = pat2beta(f'{pat_path}', self.out_dir, args=self.args)
             eprint(f'[wt bam2pat] generated {beta_path}')
@@ -298,8 +326,10 @@ def parse_bam2pat_args(parser):
                         nargs='?', const=True, default=False)
     lists.add_argument('-L', '--whitelist', help='bed file. Consider only reads overlapping this bed file',
                         nargs='?', const=True, default=False)
-    parser.add_argument('--blueprint', '-bp', action='store_true',
-            help='filter bad bisulfite conversion reads if <90 percent of CHs are converted')
+    parser.add_argument('--mbias', '-mb', action='store_true',
+            help='Output mbias plots. Only paired-end data is supported')
+    # parser.add_argument('--blueprint', '-bp', action='store_true',  # TODO put it back
+            # help='filter bad bisulfite conversion reads if <90 percent of CHs are converted')
 
 
 def add_args():
