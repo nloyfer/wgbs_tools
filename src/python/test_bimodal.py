0aad37922394abbb3ba774af5b09003904a29617
yonniejon <jonathanrosenski@gmail.com>
2021-10-24 15:25:14 +0300

    added test_bimodal functionality

--- src/python/test_bimodal.py
+++ src/python/test_bimodal.py
@@ -2,7 +2,15 @@ import argparse
 import subprocess
 import warnings
 from genomic_region import GenomicRegion
-from utils_wgbs import add_GR_args, MAX_PAT_LEN
+from utils_wgbs import add_GR_args, MAX_PAT_LEN, get_genome_name, GenomeRefPaths, add_multi_thread_args, \
+    IllegalArgumentError
+from multiprocessing import Pool
+import pandas as pd
+try:
+    from statsmodels.stats.multitest import multipletests
+except:
+    raise IllegalArgumentError("Please install statsmodels package in order to use this feature. i.e. 'pip install statsmodels'")
+
 import numpy as np
 from scipy import stats
 
@@ -74,7 +82,7 @@ def read_pat_vis(pat_text, start):
     return pat_matrix
 
 
-def em_pat_matrix(pat_matrix):
+def em_pat_matrix(pat_matrix, should_print=True):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         c_char = b'C'
@@ -133,13 +141,13 @@ def em_pat_matrix(pat_matrix):
             totals = p_c + p_t
             l_p_c = np.log2(p_c / totals)
             l_p_t = np.log2(p_t / totals)
-
-        bpi = 2 ** (ll / sum(n_per_col))
-        print(f"LL: {ll} | {num_reads} reads | {int(round(sum(n_per_col)))} observed | BPI: {bpi}")
+        if should_print:
+            bpi = 2 ** (ll / sum(n_per_col))
+            print(f"LL: {ll} | {num_reads} reads | {int(round(sum(n_per_col)))} observed | BPI: {bpi}")
         return ll
 
 
-def calc_initial_liklihood(pat_matrix):
+def calc_initial_liklihood(pat_matrix, should_print=True):
     c_per_col = 1e-3 + sum(pat_matrix == b'C')
     t_per_col = 1e-3 + sum(pat_matrix == b'T')
     n_per_col = c_per_col + t_per_col
@@ -147,7 +155,8 @@ def calc_initial_liklihood(pat_matrix):
     l_p_t = np.log2(t_per_col / n_per_col)
     ll0 = sum(sum(pat_matrix == b'C') * l_p_c + sum(pat_matrix == b'T') * l_p_t)
     bpi = 2 ** (ll0 / sum(n_per_col))
-    print(f"LL: {ll0} | {pat_matrix.shape[0]} reads | {int(round(sum(n_per_col)))} observed | BPI: {bpi}")
+    if should_print:
+        print(f"LL: {ll0} | {pat_matrix.shape[0]} reads | {int(round(sum(n_per_col)))} observed | BPI: {bpi}")
     return ll0
 
 
@@ -156,11 +165,94 @@ def pull_pat_file(region, file_name):
     return subprocess.check_output(cmd, shell=True).decode()
 
 
+def test_single_region(pat_file, chrom, region=None, sites=None, should_print=True):
+    if region is None and sites is None:
+        raise IllegalArgumentError("sites and regions cannot both be None")
+    if region is not None:
+        gr = GenomicRegion(region=region)
+        s1, s2 = gr.sites
+    else:
+        s1 = sites[0]
+        s2 = sites[1]
+    # chrom = region.split(":")[0]
+    new_start = max(1, s1 - MAX_PAT_LEN)
+    pat_region = f"{chrom}:{new_start}-{s2}"
+    pat_text = pull_pat_file(pat_region, pat_file)
+    pat_mat = read_pat_vis(pat_text, s1)
+    ll0 = calc_initial_liklihood(pat_mat, should_print=should_print)
+    post_em_ll = em_pat_matrix(pat_mat, should_print=should_print)
+    test_stat = 2 * np.log(2) * (post_em_ll - ll0)
+    pv = 1 - stats.chi2.cdf(test_stat, pat_mat.shape[1] - 1)
+    if should_print:
+        pv_str = "{:,.3e}".format(pv)
+        print(f"pvalue: {pv_str}")
+    return pv
+
+
+def read_blocks_and_test(tabixed_bed_file, cur_region, pat_file):
+    peek_df = pd.read_csv(tabixed_bed_file, sep='\t', nrows=1, header=None, comment='#')
+    names = ['chr', 'start', 'end', 'startCpG', 'endCpG']
+    if len(peek_df.columns) < len(names):
+        msg = f'Invalid blocks file: {tabixed_bed_file}. less than {len(names)} columns'
+        raise IllegalArgumentError(msg)
+    tabix_cmd = f"tabix {tabixed_bed_file} {cur_region}"
+    cur_blocks_lines = subprocess.check_output(tabix_cmd, shell=True).decode().split("\n")
+    p_val_list = []
+    for line in cur_blocks_lines:
+        if len(line) > 3:
+            tokens = line.split("\t")
+            # region_to_test = f"{tokens[0]}:{tokens[1]}-{tokens[2]}"
+            sites = (int(tokens[3]), int(tokens[4]))
+            p_val = test_single_region(pat_file, tokens[0], sites=sites, should_print=False)
+            p_val = p_val.astype(np.float32)
+            p_val_list.append((line, p_val))
+    print(f"finished processesing {cur_region}")
+    return p_val_list
+
+
+def choose_blocks_by_fdr_bh(pvals, blocks):
+    rejected_list, corrected_p_vals, _, _ = multipletests(pvals, alpha=0.05, method='fdr_bh')
+    index = 0
+    for rejected in rejected_list:
+        if not rejected:
+            break
+        index += 1
+    if index > 0:
+        return blocks[0:index], corrected_p_vals[0:index]
+    else:
+        return [], []
+
+
+def test_multiple_regions(tabixed_bed_file, pat_file, num_threads, out_file):
+    cf = GenomeRefPaths(get_genome_name(None)).get_chrom_cpg_size_table()
+    chroms = sorted(set(cf.chr))
+    params_list = []
+    for chrom in chroms:
+        params_list.append((tabixed_bed_file, chrom, pat_file))
+
+    p = Pool(num_threads)
+    arr = p.starmap(read_blocks_and_test, params_list)
+    p.close()
+    p.join()
+
+    region_p_val_list = [p_reg for x in arr for p_reg in x]
+    region_p_val_list = sorted(region_p_val_list, key=lambda elem: elem[1])
+    [block_lines, p_vals] = zip(*region_p_val_list)
+    accepted_blocks, corrected_p_vals = choose_blocks_by_fdr_bh(p_vals, block_lines)
+    with open(out_file, 'w') as f_out:
+        for accepted_block, corrected_p_val in zip(accepted_blocks, corrected_p_vals):
+            corrected_p_val = "{:,.1e}".format(corrected_p_val)
+            f_out.write(f"{accepted_block}\t{corrected_p_val}\n")
+    x = 0
+
+
+
 def add_args():
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('pat', help="The input pat file")
-    add_GR_args(parser)
-    # add_multi_thread_args(parser)
+    add_GR_args(parser, bed_file=True)
+    add_multi_thread_args(parser)
+    parser.add_argument('--out_file', help="Output file name in which to write results")
     return parser
 
 
@@ -176,19 +268,12 @@ def main():
     parser = add_args()
     args = parse_args(parser)
 
-    gr = GenomicRegion(region=args.region)
-    s1, s2 = gr.sites
-    chrom = args.region.split(":")[0]
-    new_start = max(1, s1 - MAX_PAT_LEN)
-    pat_region = f"{chrom}:{new_start}-{s2}"
-    pat_text = pull_pat_file(pat_region, args.pat)
-    pat_mat = read_pat_vis(pat_text, s1)
-    ll0 = calc_initial_liklihood(pat_mat)
-    post_em_ll = em_pat_matrix(pat_mat)
-    test_stat = 2 * np.log(2) * (post_em_ll - ll0)
-    pv = 1 - stats.chi2.cdf(test_stat, pat_mat.shape[1] - 1)
-    pv = "{:,.3e}".format(pv)
-    print(f"pvalue: {pv}")
+    if args.region is not None:
+        test_single_region(args.pat, args.region.split(":")[0], region=args.region)
+    elif args.bed_file is not None:
+        if args.out_file is None:
+            raise IllegalArgumentError("If multi-region bed file is specified, then --out_file must be specified.")
+        test_multiple_regions(args.bed_file, args.pat, args.threads, args.out_file)
 
 
 if __name__ == '__main__':
