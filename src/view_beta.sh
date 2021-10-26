6e6d95009f5ea3f9f33af412acd7a225e5c28b18
nloyfer@gmail.com <nloyfer@gmail.com>
2021-10-26 10:56:27 +0300

    shift locus back by 1

--- src/view_beta.sh
+++ src/view_beta.sh
@@ -2,13 +2,13 @@
 dict=$1
 beta=$2
 if [ "$#" -eq 2 ]; then
-    paste <(gunzip -c $dict | awk -v OFS='\t' '{print $1,$2,$2+2}') <(hexdump -v -e '/1 "%u\t" /1 "%u\n"' $beta)
+    paste <(gunzip -c $dict | awk -v OFS='\t' '{print $1,$2-1,$2+1}') <(hexdump -v -e '/1 "%u\t" /1 "%u\n"' $beta)
 elif [ "$#" -eq 5 ]; then
     chrom=$3
     start=$4
     nr_sites=$5
     end=$(( start+nr_sites-1 ))
-    paste <(tabix $dict ${chrom}":"${start}"-"${end} | awk -v OFS='\t' '{print $1,$2,$2+2}') <(hexdump -s $(( 2*(start-1) )) -n $(( 2*nr_sites )) -v -e '/1 "%u\t" /1 "%u\n"' $beta)
+    paste <(tabix $dict ${chrom}":"${start}"-"${end} | awk -v OFS='\t' '{print $1,$2-1,$2+1}') <(hexdump -s $(( 2*(start-1) )) -n $(( 2*nr_sites )) -v -e '/1 "%u\t" /1 "%u\n"' $beta)
 else
     echo "Illegal number of parameters"
 fi
