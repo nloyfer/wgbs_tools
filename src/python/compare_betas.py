72619f86e6ea242e05bad8d229aaa8f2be77568d
nloyfer@gmail.com <nloyfer@gmail.com>
2021-11-23 17:28:01 +0200

    improve >2 betas figure

--- src/python/compare_betas.py
+++ src/python/compare_betas.py
@@ -6,43 +6,59 @@ from matplotlib.colors import LogNorm
 from utils_wgbs import load_beta_data, validate_file_list, add_GR_args, eprint
 from genomic_region import GenomicRegion
 import os.path as op
-from itertools import combinations
 
 
-def comp2(a, b, ids, cov_thresh, out_dir):
+def comp2(a, b, cov_thresh, ax):
     # Ignore sites with coverage lower than cov_thresh in at
     # least one of the two compared files
     high_cov = np.min(np.c_[a[:, 1], b[:, 1]], axis=1) >= cov_thresh
     def table2vec(table):
         table = table[high_cov]
         return table[:, 0] / table[:, 1]
-    plt.hist2d(table2vec(a), table2vec(b), bins=20, cmap=plt.cm.jet, norm=LogNorm())
-    plt.title('{} : {}'.format(*ids), fontsize=10)
-    if out_dir is not None:
-        outpath = op.join(out_dir, '{}:{}.png'.format(*ids))
-        plt.savefig(outpath)
-        eprint(f'[wt cmp] dumped figure to {outpath}')
+    ax.hist2d(table2vec(a), table2vec(b), bins=20, cmap=plt.cm.jet, norm=LogNorm())
 
 
-# def compare_all_paires(betas, min_cov, sites):
 def compare_all_paires(args):
     betas = args.betas
     sites = GenomicRegion(args).sites
     tables = [load_beta_data(b, sites) for b in betas]
     names = [op.splitext(op.basename(b))[0] for b in betas]
+    # break names to lines
+    nnames = []
+    k = 20
+    for n in names:
+        lst = [n[0+i:k+i] for i in range(0, len(n), k)]
+        nn = '\n'.join(lst)
+        nnames.append(nn)
 
-    for x, y in combinations(range(len(tables)), r=2):
-        plt.figure()
-        comp2(tables[x], tables[y], (names[x], names[y]), args.min_cov, args.out_dir)
-    if args.show or args.out_dir is None:
+    N = len(tables)
+    fig, axs = plt.subplots(N, N)
+    for i in range(N):
+        for j in range(i + 1):
+            comp2(tables[i], tables[j], args.min_cov, axs[i, j])
+        axs[i, 0].set_ylabel(nnames[i], fontsize=8)
+    for j in range(N):
+        axs[0, j].set_title(nnames[j], fontsize=8)
+
+    for ax in axs.flat:
+        ax.label_outer()
+
+    fig.tight_layout()
+
+
+    if args.outpath is not None:
+        plt.savefig(args.outpath)
+        eprint(f'[wt cmp] dumped figure to {args.outpath}')
+
+    if args.show or args.outpath is None:
         plt.show()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('betas', nargs='+')
-    parser.add_argument('--out_dir', '-o',
-            help='Dump figures to this directory. If not specified, --show flag is set')
+    parser.add_argument('--outpath', '-o',
+            help='Dump figure to this path (e.g., pdf/png). If not specified, --show flag is set')
     parser.add_argument('--show', action='store_true',
             help='Display the figures using matplotlib.pyplot.show.')
     parser.add_argument('--min_cov', '-c', type=int, default=10,
@@ -62,7 +78,6 @@ def main():
     """
     args = parse_args()
     validate_file_list(args.betas, '.beta', min_len=2)
-    # compare_all_paires(args.betas, args.min_cov, GenomicRegion(args).sites)
     compare_all_paires(args)
 
 
