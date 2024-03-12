9e96cc98e1fe37d0e06c080c5d12ccfb502c6dcb
nloyfer@gmail.com <nloyfer@gmail.com>
2024-03-12 12:34:30 +0200

    compile mask_pat with utils

--- setup.py
+++ setup.py
@@ -40,7 +40,10 @@ def compile_single(cmd, name, verbose):
 
 modules = {
         'stdin2beta'    : 'g++ -std=c++11 src/pat2beta/stdin2beta.cpp -o src/pat2beta/stdin2beta',
-        'mask_pat'      : 'g++ -std=c++11 src/pat2beta/mask_pat.cpp -o src/pat2beta/mask_pat',
+        # 'mask_pat'      : 'g++ -std=c++11 src/pat2beta/mask_pat.cpp -o src/pat2beta/mask_pat',
+        'mask_pat'      : 'g++ -std=c++11 -c -o src/pat2beta/mask_pat.o src/pat2beta/mask_pat.cpp && '\
+                          'g++ -std=c++11 -c -o src/pipeline_wgbs/patter_utils.o src/pipeline_wgbs/patter_utils.cpp && '\
+                          'g++ -std=c++11 -o src/pat2beta/mask_pat src/pat2beta/mask_pat.o src/pipeline_wgbs/patter_utils.o',
         # 'stdin2pairs'   : 'g++ -std=c++11 src/pat2beta/stdin2pairs.cpp -o src/pat2beta/stdin2pairs',
         'pat_sampler'   : 'g++ -std=c++11 src/pat_sampler/sampler.cpp -o src/pat_sampler/pat_sampler',
         'patter'        : 'g++ -std=c++11 -c -o src/pipeline_wgbs/main.o src/pipeline_wgbs/main.cpp && '\
