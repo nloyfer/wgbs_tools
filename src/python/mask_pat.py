100e498d25170ac303fd49d115706079b0efb05d
nloyfer@gmail.com <nloyfer@gmail.com>
2024-07-15 11:31:05 +0300

    add beta flag to mask_pat

--- src/python/mask_pat.py
+++ src/python/mask_pat.py
@@ -2,10 +2,12 @@
 
 import argparse
 import subprocess
+import os.path as op
 from genomic_region import GenomicRegion
+from pat2beta import pat2beta
 from utils_wgbs import validate_single_file, mask_pat_tool, \
         delete_or_skip, main_script, collapse_pat_script, \
-        add_GR_args
+        add_GR_args, eprint, add_multi_thread_args
 
 
 def mask_pat(pat_path, sites_to_hide, prefix, args):
@@ -28,6 +30,9 @@ def mask_pat(pat_path, sites_to_hide, prefix, args):
     cmd += f' | bgzip -@ 4 > {pat_out}'
     cmd += f' && {main_script} index {pat_out}'
     subprocess.check_call(cmd, shell=True)
+    if args.beta or args.lbeta:
+        eprint(f'generate {"l" if args.lbeta else ""}beta file')
+        pat2beta(pat_out, op.dirname(pat_out), args=args, force=True)
     return pat_out
 
 
@@ -35,6 +40,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description=main.__doc__)
     add_GR_args(parser, bed_file=True)
     parser.add_argument('pat_path', help='pat file')
+    add_multi_thread_args(parser)
+    # mutually exclusive beta or lbeta:
+    beta_or_lbeta = parser.add_mutually_exclusive_group(required=False)
+    beta_or_lbeta.add_argument('--beta', action='store_true', help='create beta from output pat')
+    beta_or_lbeta.add_argument('--lbeta', action='store_true', help='create lbeta from output pat')
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite existing file if existed')
     parser.add_argument('-p', '--prefix', help='Output prefix for the masked pat file', required=True)
     parser.add_argument('--sites_to_hide', '-b', help='bed file with sites / blocks to mask out.\n'
